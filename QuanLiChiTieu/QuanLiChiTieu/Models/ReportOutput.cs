1e668615b26fc68d7781a193a9b3f6397ce065c7
phuongtnd1204 <phuongtnd1204@gmail.com>
2019-01-03 08:22:23 +0700

    Commit DB

---
+++ QuanLiChiTieu/QuanLiChiTieu/Models/ReportOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLiChiTieu.Models
+{
+    public class ReportOutput
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Sum { get; set; }
+    }
+}
