5bf9b763a8bebfa0f737b5f0aa941d8faf3512a3
phuongtnd1204 <phuongtnd1204@gmail.com>
2019-01-07 09:08:01 +0700

    commit 01/07/019

--- QuanLiChiTieu/QuanLiChiTieu.Android/MainActivity.cs
+++ QuanLiChiTieu/QuanLiChiTieu.Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.Media;
 using Prism;
 using Prism.Ioc;
 
@@ -9,15 +10,23 @@ namespace QuanLiChiTieu.Droid
     [Activity(Label = "QuanLiChiTieu", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
+            await CrossMedia.Current.Initialize();
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
